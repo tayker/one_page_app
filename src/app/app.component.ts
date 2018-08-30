44eca7d670ec99a8437c5746f332f7d6f2144683
sergey.front <sergeyfront@hitech18.online>
2018-08-30 11:16:28 +0300

    add router preloading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
-import { fadeAnimation } from '../animations';
+import {
+  Event as RouterEvent,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [fadeAnimation]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  isLoaded: boolean = true;
+  isLoaded = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+            this.isLoaded = true;
+          break;
+        }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          setTimeout(() => {
+            this.isLoaded = false;
+          }, 2000);
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+  }
 }
